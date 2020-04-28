9c80a5339e7976244e03cd0d77caca57a094b8df
Josue Romero <jcromero@pucp.edu.pe>
2020-04-27 20:35:19 -0500

    josue new form

--- src/main/java/lab2/sw2/Controller/JobsController.java
+++ src/main/java/lab2/sw2/Controller/JobsController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,10 +27,24 @@ public class JobsController {
         return "job/lista";
     }
 
-    @GetMapping("/new")
-    public String newEmployee(Model model){
-        model.addAttribute("listaJob",jobsRepository.findAll());
-        return "Employee/newForm";
+    @GetMapping("/nuevo")
+    public String nuevoJob(){
+        return "job/newForm";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(JobsEntity joobs, RedirectAttributes redirectAttributes){
+        if(joobs.getJob_id()!=null){
+            if(joobs.getMin_salary() >0 && joobs.getMax_salary()>0){
+                redirectAttributes.addFlashAttribute("mensaje","JOB Creado Exitosamente");
+                jobsRepository.save(joobs);
+                return "redirect:/listaJ";
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("mensaje","JOB Actualizado Exitosamente");
+        }
+        jobsRepository.save(joobs);
+        return "redirect:/listaJ";
     }
 
 
